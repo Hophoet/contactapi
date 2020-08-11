7bd04d62443da7cc608107d6c2dd7c19c18822bc
hophoet <hophoet@gmail.com>
2020-08-11 05:38:34 +0000

    update urls.py

--- core/urls.py
+++ core/urls.py
@@ -7,7 +7,7 @@ app_name = 'core'
 
 #urls root
 urlpatterns = [
-    path('', views.ContactsListView.as_view(), name='contacts'),#contact listing view
+    path('', views.ContactsListView.as_view(), name='contacts_list'),#contact listing view
     path('contact/<int:client_id>/', views.ContactDetailView.as_view(), name='detail'),#contact detail view
     path('contact', views.ClientSearchView.as_view(), name='search_contact'),#client searching view
 ]
